0d826b335c467992341251755505b64c795c34b4
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-06-14 22:59:26 -0400

    fixed link

--- src/app/sums-below-one/statistics/page.tsx
+++ src/app/sums-below-one/statistics/page.tsx
@@ -7,7 +7,7 @@ const Page: React.FC = () => {
       <h1 className="text-3xl font-bold text-center mb-10">
         Statistics of game
       </h1>
-      <Link href="/sums-over-one">
+      <Link href="/sums-below-one">
         <p className="inline-block bg-green-700 text-white font-semibold px-4 py-2 rounded-full hover:bg-blue-600 transition duration-300 ease-in-out">
           Go back to game
         </p>
