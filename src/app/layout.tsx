a62c4ba1fc95da032c319bf625f243988cbce5aa
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-06-14 20:42:59 -0400

    google analytics added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
-import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Providers from "./providers"; //import the providers component
 import TailwindCookieConsent from "./components/TailwindCookieConsent/TailwindCookieConsent"; //cookies consent
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
+
+const googleAnalyticsId = process.env.NEXT_PUBLIC_GA_ID as string;
 
 export default function RootLayout({
   children,
@@ -18,7 +20,7 @@ export default function RootLayout({
       <head>
         <Script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-RHRHJWFQ5M"
+          src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
         ></Script>
         <Script id="google-analytics">
           {`
@@ -26,7 +28,7 @@ export default function RootLayout({
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-RHRHJWFQ5M');
+  gtag('config', ${googleAnalyticsId});
     `}
         </Script>
       </head>
