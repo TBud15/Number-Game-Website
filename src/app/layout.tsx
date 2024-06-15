1f2627162c114ee5b972e0c49d7c8a50ad37979a
Fluffy1908 <pro1shinov.tima@gmail.com>
2024-06-14 20:55:49 -0400

    google analytics aproach fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,9 @@ import Navbar from "@/app/components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Providers from "./providers"; //import the providers component
 import TailwindCookieConsent from "./components/TailwindCookieConsent/TailwindCookieConsent"; //cookies consent
-import Script from "next/script";
+import GoogleAnalytics from "./GoogleAnalytics/GoogleAnalytics";
 
 const inter = Inter({ subsets: ["latin"] });
-
-const googleAnalyticsId = process.env.NEXT_PUBLIC_GA_ID as string;
 
 export default function RootLayout({
   children,
@@ -17,21 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <Script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
-        ></Script>
-        <Script id="google-analytics">
-          {`
-      window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', ${googleAnalyticsId});
-    `}
-        </Script>
-      </head>
+      <GoogleAnalytics />
       <body className={inter.className}>
         <Providers>
           <Navbar />
